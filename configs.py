4659e4550e18601bc5e59f9de727a7a1d0699cbe
nazifasumaiya <nazifasumaiya28@gmail.com>
2025-01-10 03:07:51 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ class Config(object):
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002450723292"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "7273593616"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://AniReal123:AniReal123@AniReal123.hetez.mongodb.net/?retrywrite=true&w=majority&appName=AniReal123")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002409521763")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002335115531"))
