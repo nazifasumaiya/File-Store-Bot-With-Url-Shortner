1a8691ff7eb0bda43f625e109cb048a1e5599ca4
nazifasumaiya <nazifasumaiya28@gmail.com>
2025-01-11 01:23:11 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,11 +32,9 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/not_name_not_username)
  
  I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
